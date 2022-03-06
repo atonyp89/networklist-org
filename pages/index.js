095705a7b0679ceb6adb9fafdb3c0a66f64095a0
tuanphung <tuanphunglk@gmail.com>
2022-03-06 23:03:29 +0700

    remove multichain ads

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import {
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import Chain from '../components/chain'
-import MultiChain from '../components/multichain'
+// import MultiChain from '../components/multichain'
 import Header from '../components/header'
 
 import SearchIcon from '@material-ui/icons/Search';
@@ -85,7 +85,7 @@ function Home({ changeTheme, theme }) {
 
   const [ layout, setLayout ] = useState('grid')
   const [ search, setSearch ] = useState('')
-  const [ hideMultichain, setHideMultichain ] = useState('1')
+  // const [ hideMultichain, setHideMultichain ] = useState('1')
   const router = useRouter()
   if (router.query.search) {
     setSearch(router.query.search)
@@ -107,19 +107,19 @@ function Home({ changeTheme, theme }) {
     window.open('https://github.com/ethereum-lists/chains', '_blank')
   }
 
-  const closeMultichain = (perma) => {
-    setHideMultichain('1')
-    localStorage.setItem('chainlist.org-hideMultichain', perma ? '1' : '0')
-  }
+  // const closeMultichain = (perma) => {
+  //   setHideMultichain('1')
+  //   localStorage.setItem('chainlist.org-hideMultichain', perma ? '1' : '0')
+  // }
 
-  useEffect(() => {
-    const multi = localStorage.getItem('chainlist.org-hideMultichain')
-    if(multi) {
-      setHideMultichain(multi)
-    } else {
-      setHideMultichain('0')
-    }
-  }, [])
+  // useEffect(() => {
+  //   const multi = localStorage.getItem('chainlist.org-hideMultichain')
+  //   if(multi) {
+  //     setHideMultichain(multi)
+  //   } else {
+  //     setHideMultichain('0')
+  //   }
+  // }, [])
 
   return (
     <div className={styles.container}>
@@ -185,7 +185,7 @@ function Home({ changeTheme, theme }) {
               <Header changeTheme={ changeTheme } />
             </div>
             <div className={ classes.cardsContainer }>
-              { hideMultichain === '0' && <MultiChain closeMultichain={ closeMultichain } /> }
+              {/* { hideMultichain === '0' && <MultiChain closeMultichain={ closeMultichain } /> } */}
               {
                 data && data.filter((chain) => {
                   if(search === '') {
